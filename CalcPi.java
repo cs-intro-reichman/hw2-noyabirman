7f34f2781bd088c690048815ce0876388b916d3d
Noya Birman <noya.birman@post.runi.ac.il>
2024-11-18 20:10:25 +0200

    compketed

--- CalcPi.java
+++ CalcPi.java
@@ -12,7 +12,7 @@ public class CalcPi {
 			
 		}
 		System.out.println("pi according to Java: "+(sum*4));
-		System.out.println("pi, approximated: "+Math.PI);
+		System.out.println("pi, approximated:     "+Math.PI);
 
 
 	}
