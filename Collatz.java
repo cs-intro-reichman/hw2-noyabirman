f513f7f358d44d8dde62da357421c46d2d6c3deb
Noya Birman <noya.birman@post.runi.ac.il>
2024-11-21 17:17:49 +0200

    completed

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,47 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int N= Integer.parseInt(args[0]);
+		boolean reached1=true;
+		String mode=args[1];
+		
+		for (int seed=1;seed<=N;seed++){
+			int num=seed;
+			int totalSteps=0;
+			
+			if (mode.equals("v")) {
+				System.out.print((seed)+ " ");	
+			}
+
+			while (num!=1){
+				if (num%2==0){
+					num/=2;
+				} else {
+					num=num*3+1;
+				}
+				totalSteps++;
+				
+				if (mode.equals("v")) {
+					System.out.print(num+ " ");
+				}
+			}
+			if (mode.equals("v")) {
+				System.out.println("(" + totalSteps + ")");	
+			}
+			if (num != 1){
+				reached1=false;
+			}
+		}	
+		if (reached1) {
+			System.out.println("Every one of the first "+ N+ "hailstone sequences reached 1.");
+
+		}
+		
+		}	
 	}
-}
+
+	
+	
+
+	
+
